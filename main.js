b2f6f3ec96449cd707025ec93d3c61f880bd26be
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-10-29 18:01:31 -0700

    Remove unnecessary commented code

--- main.js
+++ main.js
@@ -8,13 +8,8 @@
     $('.box').on('click.mark', (event) => {
       let el = event.target;
 
-      //console.log(lastMark)
-      // console.log( el.id );
-
       lastMark = (lastMark === "X") ? "O" : "X";
-
       $(el).text(lastMark);
-
       $(el).off('click.mark');
     });
 
