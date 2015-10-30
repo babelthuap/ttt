a320264d8a6e6a350d72b6b9d102a41d524f140f
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-10-29 19:26:51 -0700

    Add ability to win and reset game

--- main.js
+++ main.js
@@ -1,15 +1,51 @@
 (function app() {
   'use strict';
 
-  let documentReady = () => {
 
+  let documentReady = () => {
     let lastMark;
+
+    let winningStates = [ [1,2,3], [4,5,6], [7,8,9],
+                          [1,4,7], [2,5,8], [3,6,9],
+                          [1,5,9], [3,5,7] ];
+
+    let positionsOfX = [];
+    let positionsOfO = [];
+
+    Array.prototype.containedIn = function(otherArray) {
+      // every element of A is and element of B
+      return this.every(function(element) {
+        return otherArray.indexOf(element) !== -1;
+      });
+    }
+
+    // check for a winning condition
+    function isWinner( positions ) {
+      return winningStates.some(function(winningState) {
+        return winningState.containedIn( positions );
+      });
+    }
 
     $('.box').on('click.mark', (event) => {
       let el = event.target;
 
       lastMark = (lastMark === "X") ? "O" : "X";
       $(el).text(lastMark);
+
+      if (lastMark === "X") {
+        positionsOfX.push( +el.id );
+        if (isWinner(positionsOfX)) {
+          $('#winner').text('X wins!');
+          $('.box').off('click.mark');
+        }
+      } else {
+        positionsOfO.push( +el.id );
+        if (isWinner(positionsOfO)) {
+          $('#winner').text('O wins!');
+          $('.box').off('click.mark');
+        }
+      }
+
       $(el).off('click.mark');
     });
 
@@ -17,5 +53,10 @@
 
 
   $(documentReady);
+
+  $('#restart').click(function(){
+    $('.box').empty();
+    documentReady();
+  })
 
 })();
